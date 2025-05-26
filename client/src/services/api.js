9c12ec70e2703a879556cd940f504f47ee318bec
harsh-1542 <harshshrimali0002@gmail.com>
2025-05-26 16:15:11 +0530

    last commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://quotation-management-bxu8.vercel.app/api', // Your backend API URL
+  baseURL: 'https://quotation-management.onrender.com/api', // Your backend API URL
   headers: {
     'Content-Type': 'application/json',
   },
